c5cc6244b20d225886c3338b07a36196f3dbbd9b
kingjakeu <jakeyoo12@gmail.com>
2020-10-01 20:04:04 +0900

    Solve Q215

---
+++ leetcode/medium/Q215_KthLargestElementinanArray.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return -1
+        nums.sort()
+        return nums[-k]
